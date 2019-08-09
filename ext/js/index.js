596b9923409de7ef6ce6664bbcefee338234ccdb
Jorge Madrigal Adan <jorgemadrigaladan@gmail.com>
2019-08-09 15:31:30 -0700

    Final Version of Chrome Extension

---
+++ ext/js/index.js
@@ -0,0 +1,29 @@
+// document.querySelector("#button").addEventListener("click", link),;
+
+// document.querySelector("#button").addEventListener("click", () => link());
+
+// document.querySelector("#button").addEventListener("click", function() {
+//   link();
+// });
+
+document
+  .querySelector("#button")
+  .addEventListener("click", () => window.open("http://www.spotify.com"));
+
+// document
+//   .querySelector("#button")
+//   .addEventListener(
+//     "click",
+//     window.open.bind(window, "http://www.spotify.com")
+//   );
+
+
+
+
+//   document.querySelector("#button").addEventListener("click", () => {
+//   document.getElementById("button").onclick = link();
+// });
+
+// function link() {
+//   window.open("http://www.spotify.com");
+// }
